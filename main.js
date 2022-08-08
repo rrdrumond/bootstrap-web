87000287d679f59ec3fda4b710e2428898a2b1af
rrdrumond <rrdrumond@gmail.com>
2022-08-08 20:31:23 +0100

    Add main.js

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ ScrollReveal().reveal('.header-content-left', { distance: '300px', duration: 200
 ScrollReveal().reveal('.header-content-right', { distance: '300px', duration: 2000, origin: 'right' });
 ScrollReveal().reveal('.header-btn', { delay: 1000, duration: 2000, origin: 'bottom' });
 ScrollReveal().reveal('#testimonial', { distance: '300px', duration: 2000, origin: 'left', viewFactor: 0.2});
+ScrollReveal().reveal('.header-btn', { delay: 1000, duration: 2000, origin: 'bottom' });
+ScrollReveal().reveal('.info-left', { distance: '300px', duration: 2000, origin: 'bottom' });
+ScrollReveal().reveal('.info-right', { distance: '300px', duration: 2000, origin: 'top' });
+ScrollReveal().reveal('#extra-info', { delay: 1000, duration: 2000, origin: 'top', viewFactor:0.2 });
+ScrollReveal().reveal('#contact', { delay: 500, duration: 2000, origin: 'top', viewFactor: 0.2 });
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
